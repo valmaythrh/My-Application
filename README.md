51f5de2420c4aae423b342a47b5588ad0473feda
Subhan Nooriansyah <subhan.nooriansyah@gmail.com>
2018-07-11 07:18:26 +0700

    fixup splashscreen and what next step??

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # My-Application
+
+##Todo List
+
+<h1> Nama Package Tidak Sesuai </h1>
+
+<ul> 
+    <li> ganti nama package android / domain menjadi <strong> lave.id </strong> </li>
+    <li> pastikan semua sudah diganti dengan lave.id </li>
+</ul>
+
+<h1> Buang File Memberatkan Proyek </h1>
+
+<ul>
+    <li> begron.png </li> 
+    <li> begronnn.png </li>
+    <li> ganti dengan code warna contoh #FFFF (white)</li>
+</ul>
+
+<h1> What the next???  </h1>
+<ul>
+    <li> Tulis Fitur-fitur yang akan dibuat dalam bentuk .txt kirim via email </li>
+</ul>
+
+<h1> Update Proyek  </h1>
+<ul>
+    <li> git pull origin master </li>
+    <li> google atau youtube menggunakan <b>git version control on windows / android</b> </li>
+</ul>
--- app/src/main/java/com/example/d0n1p/myapplication/ActivitySplashScreen.java
+++ app/src/main/java/com/example/d0n1p/myapplication/ActivitySplashScreen.java
@@ -1,37 +1,31 @@
 package com.example.d0n1p.myapplication;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class ActivitySplashScreen extends AppCompatActivity {
-    private int SLEEP_TIMER = 3;
+
+    private int SLEEP_TIMER = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.splash_screen);
-        getSupportActionBar().hide();
-        LogoLauncher logoLauncher = new LogoLauncher();
-        logoLauncher.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iSplash = new Intent(ActivitySplashScreen.this, MainActivity.class);
+                startActivity(iSplash);
+
+                finish();
+            }
+        }, SLEEP_TIMER);
 
     }
-    private class LogoLauncher extends Thread{
-        public void run(){
-            try{
-                sleep(1000 * SLEEP_TIMER);
-            }catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            Intent intent = new Intent(ActivitySplashScreen.this, MainActivity.class);
-            startActivity(intent);
-            ActivitySplashScreen.this.finish();
-        }
-    }
+
 }
